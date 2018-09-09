4f4b43a53ee9c4ef6277bd0b3fe9c01494fb5700
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-09 16:11:45 +0530

    Activity Main Ui

--- app/src/main/java/com/sagar/visualpolish/MainActivity.java
+++ app/src/main/java/com/sagar/visualpolish/MainActivity.java
@@ -2,6 +2,7 @@ package com.sagar.visualpolish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Four text views for four activities
+        TextView colorAndFont = findViewById(R.id.colorAndFont);
+        TextView style = findViewById(R.id.style);
+        TextView responsiveLayouts =  findViewById(R.id.responsiveLayouts);
+        TextView touchSelector =  findViewById(R.id.touchSelector);
+
+        // end
     }
+
+
+    // END
 }
