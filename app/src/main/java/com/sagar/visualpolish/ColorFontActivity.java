5a8f73f4b86f13df0798c2754d254d6c106b3488
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-09 16:31:39 +0530

    ColorFontActivity

---
+++ app/src/main/java/com/sagar/visualpolish/ColorFontActivity.java
@@ -0,0 +1,13 @@
+package com.sagar.visualpolish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorFontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_font);
+    }
+}
