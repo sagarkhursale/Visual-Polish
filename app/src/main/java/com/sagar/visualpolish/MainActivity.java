ff3ea0881b46c8756874884775c8d68617fe9558
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-09 18:18:07 +0530

    update MainActivity.java

--- app/src/main/java/com/sagar/visualpolish/MainActivity.java
+++ app/src/main/java/com/sagar/visualpolish/MainActivity.java
@@ -1,11 +1,14 @@
 package com.sagar.visualpolish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +18,45 @@ public class MainActivity extends AppCompatActivity {
         // Four text views for four activities
         TextView colorAndFont = findViewById(R.id.colorAndFont);
         TextView style = findViewById(R.id.style);
-        TextView responsiveLayouts =  findViewById(R.id.responsiveLayouts);
-        TextView touchSelector =  findViewById(R.id.touchSelector);
+        TextView responsiveLayouts = findViewById(R.id.responsiveLayouts);
+        TextView touchSelector = findViewById(R.id.touchSelector);
+
+
+        assert colorAndFont != null;
+        colorAndFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent numbersIntent = new Intent(MainActivity.this, ColorFontActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        assert style != null;
+        style.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this, StyleActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        assert responsiveLayouts != null;
+        responsiveLayouts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent familyIntent = new Intent(MainActivity.this, ResponsiveLayoutActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        assert touchSelector != null;
+        touchSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent colorsIntent = new Intent(MainActivity.this, SelectorActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
 
         // end
     }
